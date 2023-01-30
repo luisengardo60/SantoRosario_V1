ffc2b9f30efa8cae011a3d479a13c55da8d53194
luisengardo <luisengardo60@gmail.com>
2023-01-30 14:49:53 -0400

    first commit

---
+++ html/miprimerC.c
@@ -0,0 +1,13 @@
+/*
+gcc => es el compilador, GNU Richard Stallman
+gcc: fuente-> objeto -> ejecutable
+>Archivos de encabezado*/
+#include <stdio.h>
+int main()
+{
+printf("Hola mundo");
+printf("\n");
+system("pause");
+return 0;
+}
+
